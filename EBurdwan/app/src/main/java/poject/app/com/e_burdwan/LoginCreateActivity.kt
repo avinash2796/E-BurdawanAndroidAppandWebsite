bea7ad3a5de9e367dcfdd01a6335f0fd97f3e278
Avinash Singh <coolavi2796@gmail.com>
2017-11-13 22:42:41 +0530

    Added E-Burdwan App Basics
    Includes main activity and sign in screen

---
+++ EBurdwan/app/src/main/java/poject/app/com/e_burdwan/LoginCreateActivity.kt
@@ -0,0 +1,12 @@
+package poject.app.com.e_burdwan
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginCreateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_create)
+    }
+}
---
+++ EBurdwan/app/src/main/java/poject/app/com/e_burdwan/MainActivity.kt
@@ -0,0 +1,26 @@
+package poject.app.com.e_burdwan
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    var addPlacesButton: Button?=null
+    var addReviewsButton: Button?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        addPlacesButton=findViewById(R.id.addPlaces)
+        addPlacesButton?.setOnClickListener({
+            var clickIntent=Intent(this@MainActivity,LoginCreateActivity::class.java)
+            startActivity(clickIntent)
+        })
+        addReviewsButton=findViewById(R.id.addReviews)
+        addReviewsButton?.setOnClickListener({
+            var clickIntent=Intent(this@MainActivity,LoginCreateActivity::class.java)
+            startActivity(clickIntent)
+        })
+    }
+}
